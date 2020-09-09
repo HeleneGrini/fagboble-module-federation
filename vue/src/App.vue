f51d8660f495b27f98416a9053e9f190893189eb
Helene Grini <helenegrini96@gmail.com>
2020-09-09 19:04:36 +0200

    Add working vue app

---
+++ vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>Vue App</h3>
+    <div>{{ count }}</div>
+    <Content />
+    <Button v-bind:onClick="inc" />
+  </div>
+</template>
+
+<script>
+import { ref, defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    Content: defineAsyncComponent(() => import("./components/Content")),
+    Button: defineAsyncComponent(() => import("./components/Button")),
+  },
+
+  setup() {
+    const count = ref(0);
+    const inc = () => {
+      count.value++;
+    };
+    return {
+      count,
+      inc,
+    };
+  },
+};
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ vue/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = (env = {}) => ({
+  mode: "development",
+  cache: false,
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    compress: true,
+    port: 3001,
+    hot: true,
+  },
+  target: "web",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  resolve: {
+    extensions: [".vue", ".jsx", ".js", ".json"],
+    alias: {
+      // this isn't technically needed, since the default `vue` entry for bundlers
+      // is a simple `export * from '@vue/runtime-dom`. However having this
+      // extra re-export somehow causes webpack to always invalidate the module
+      // on the first HMR update and causes the page to reload.
+      vue: "@vue/runtime-dom",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "layout",
+      library: { type: "var", name: "layout" },
+      filename: "remoteEntry.js",
+      exposes: { App: "./src/App.vue" },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+    new VueLoaderPlugin(),
+  ],
+});
